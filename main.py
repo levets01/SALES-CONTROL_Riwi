8ac7712ff67aada3274ace1754a948044da25963
ERROR_404 <68274153+levets01@users.noreply.github.com>
2026-03-20 13:22:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,81 @@
+from funciones import registroCLientes
+from funciones import registroProductos
+from funciones import pedido
+from funciones import mostrar_ingresos
+from funciones import resumen_general
+from funciones import ver_ingresos_totales
+from funciones import limpiar_pantalla
+from funciones import pausar
+import os 
 
+from menu import menu_principal
+
+def main():
+    mis_clientes = {}
+    mis_productos = {}
+    pedidos = {}
+
+    while True: 
+        #limpiar_pantalla()
+        print(menu_principal())
+
+        opcion = menu_principal()
+
+        if opcion == "1":
+            print("=== REGISTRAR CLIENTE ===")
+            registroCLientes()
+            #clientes = registroCLientes(clientes, cid, nombre, correo)
+            print("Cliente registrado ✔")
+            pausar()
+            continue
+
+        elif opcion == "2":
+            print("=== REGISTRAR PRODUCTO ===")
+            registroProductos()
+            
+
+            #productos = registroProductos(productos, pid, nombre, precio)
+            print("Producto registrado ✔")
+            pausar()
+            continue
+        
+        elif opcion == "3":
+            print("=== CREAR PEDIDO ===")
+            pedido()
+
+        
+            #pedidos = pedido(pedidos, pedido_id, cliente, producto, cantidad, precio)
+            print("Pedido creado ✔")
+            pausar()
+            continue
+
+        elif opcion == "4":
+            mostrar_ingresos()
+            pausar()
+            continue
+
+        elif opcion == "5":
+            ingresos = ver_ingresos_totales(pedidos)
+            print(f"\nTotal ingresos: {ingresos}")
+            pausar()
+            continue
+
+        elif opcion == "6":
+            print(resumen_general(pedidos))
+            pausar()
+            continue
+
+        elif opcion == "7":
+            print("Saliendo...")
+            break
+        
+
+        else:
+            print("Opción inválida")
+            pausar()
+
+
+
+
+
+main()
